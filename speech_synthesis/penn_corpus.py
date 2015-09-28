b0fc11f654dd749bde3518498b0e0e38e7072a8e
julian121266 <rtZ678Iob>
2015-09-28 17:53:44 +0200

    Fixed an error in the creation of the vocabulary dictionary. Now training seems to run more smoothly and correctly. Final results still outstanding whether all bugs are fixed.

--- speech_synthesis/penn_corpus.py
+++ speech_synthesis/penn_corpus.py
@@ -51,9 +51,12 @@ class One_hot(DataIterator):
         # for data in self.iter(handler):
         for data in self.iter(handler, verbose=verbose):
             for name in self.vocab_size_dict.keys():
-                new_data = np.eye(self.vocab_size_dict[name])[data[name]-1]
+                # print(data[name].shape)
+                new_data = np.eye(self.vocab_size_dict[name], dtype=np.bool)[data[name]]
+                # print(new_data.shape)
                 new_data = np.squeeze(new_data)
                 data[name] = new_data
+                # print(data[name][0])
                 yield data
 
 # ------------------------------ Get the data ------------------------------- #
@@ -72,7 +75,7 @@ print(len(vocab_dict))
 
 
 # print(vocab_dict[49])
-vocab_dict.update({'\n': 50})
+vocab_dict.update({'\n': 0})
 print(vocab_dict['\n'])
 print(len(vocab_dict))
 print(vocab_dict)
@@ -83,64 +86,48 @@ test = '../data/data/ptb.char.test.txt'
 size_batch = 100
 
 def prepare_data(datafilepath, vocab_dict, maxlen, size_batch=100):
-    n_classes = len(vocab_dict)
-    # nlines = sum(1 for line in open(datafilepath, 'r'))
+    # n_classes = len(vocab_dict)
     datafile = open(datafilepath, 'r').read()
     data_new = datafile.replace(' ', '')
-    print(data_new[100:120])
+    print(data_new[0:200])
     data = np.array([vocab_dict[char] for char in data_new], dtype=int)
     # data = [data[i:i+size_batch] for i in range(0, len(data), size_batch)]
     reverse_dict = dict([(v, k) for (k, v) in vocab_dict.items()])
-    print(reverse_dict)
+    # print(reverse_dict)
     data_reverse = [reverse_dict[char] for char in data]
-    print(data_reverse[0:20])
-    # data = 0*np.ones((maxlen, nlines, 1), dtype=int)
+    # print(data_reverse[0:20])
     data_mask = np.ones(data.shape)
     data_targets = data[1:]  # one step ahead
     data = data[0:-1]  # make same size as targets (last data point cannot predict anything)
     data_mask = data_mask[0:-1]
-
-    print(data.shape)
-    # i_line = 0
-    # for line in datafile:
-    #     new_data = np.array([vocab_dict[char] for char in line.split()])
-    #     data[0:new_data.shape[0], i_line, 0] = new_data
-    #     data_mask[0:new_data.shape[0], i_line, 0] = 1
-    #     i_line += 1
     N_batch = data.shape[0]//size_batch
-    # print(N_batch*size_batch)
-    # print(maxlen*nlines//size_batch)
-    # print(data[0:N_batch*size_batch].shape)
-    # print('before')
-    # print(data[0:20])
-    # ITERATOR SOLUTION:
+
     new_data = np.zeros((size_batch, N_batch, 1), dtype=int)
     new_target = np.zeros((size_batch, N_batch, 1), dtype=int)
     new_mask = np.zeros((size_batch, N_batch, 1), dtype=int)
+    print(data[0])
     for i in range(N_batch):
         for j in range(size_batch):
+            # print(i)
+            # print(j)
+            # print(j+i*size_batch)
             new_data[j, i, 0] = data[j+i*size_batch]
             new_mask[j, i, 0] = data_mask[j+i*size_batch]
             new_target[j, i, 0] = data_targets[j+i*size_batch]
     data = new_data
-    # data = data[0:N_batch*size_batch].reshape((size_batch, N_batch, 1))
-    # print('after')
-    # print(data[0:20, 0])
-    # print(new_data[0:20, 0])
-    data_reverse = [reverse_dict[char] for char in np.squeeze(data[:, 1])]
-    print(data_reverse[0:20])
+    # print(data[0])
+    # print(data[:, 0])
+    data_reverse = [reverse_dict[char] for char in np.squeeze(np.append(data[:, 0], data[:, 1]))]
+    print(''.join(data_reverse[0:200]))
     data_mask = new_mask  #data_mask[0:N_batch*size_batch].reshape((size_batch, N_batch, 1))
     data_targets = new_target  #data_targets[0:N_batch*size_batch].reshape((size_batch, N_batch, 1))
-
-    print(data.shape)
-    print(data_targets.shape)
-
-    assert(data.shape == data_targets.shape)
-    assert(data.shape == data_mask.shape)
-    assert(len(data.shape) == 3)
-    assert(len(data_targets.shape) == 3)
-    assert(len(data_mask.shape) == 3)
-
+    # print(data.shape)
+    # print(data_targets.shape)
+    # assert(data.shape == data_targets.shape)
+    # assert(data.shape == data_mask.shape)
+    # assert(len(data.shape) == 3)
+    # assert(len(data_targets.shape) == 3)
+    # assert(len(data_mask.shape) == 3)
     return data, data_targets, data_mask
 
 
@@ -155,22 +142,22 @@ print('nclasses')
 print(n_classes)
 inp, out = bs.get_in_out_layers_for_classification(n_classes, n_classes, outlayer_name='out',
                                                    mask_name='mask')
-# inp >> bs.layers.Lstm(1000, name='lstm') >> out
+inp >> bs.layers.Lstm(1000, name='lstm') >> out
 # CLOCKWORK RNN:
-size_rnn = 1000
-timing = 1 * np.ones(100, dtype=int)
-for i in range(1, 10):
-    print(i)
-    timing = np.concatenate((timing, pow(2, i) * np.ones(100, dtype=int)), axis=0)
-print(timing) # Cw_Rnn
-inp >> bs.layers.Clockword_Rnn(size_rnn, timing, name='lstm') >> out
+# size_rnn = 1000
+# timing = 1 * np.ones(100, dtype=int)
+# for i in range(1, 10):
+    # print(i)
+    # timing = np.concatenate((timing, pow(2, i) * np.ones(100, dtype=int)), axis=0)
+# print(timing) # Cw_Rnn
+# inp >> bs.layers.Clockword_Rnn(size_rnn, timing, name='lstm') >> out
 #inp >> bs.layers.FullyConnected(100, activation_function='linear') >> out
-# network = bs.Network.from_layer(out)
-network = bs.Network.from_hdf5('penn_corpus_best_lstm_33_cont.hdf5')
+network = bs.Network.from_layer(out)
+# network = bs.Network.from_hdf5('penn_corpus_best_lstm_33_cont5.hdf5')
 
 network.set_memory_handler(PyCudaHandler())
 # network.initialize({"default": bs.Gaussian(0.1), "lstm": {'bf': 2, 'bi': 2, 'bo': 2}}, seed=42)
-# network.initialize({"default": bs.Gaussian(0.1)}, seed=42)
+network.initialize({"default": bs.Gaussian(0.1)}, seed=42)
 network.set_gradient_modifiers({"lstm": bs.ClipValues(low=-1., high=1)})
 
 # ---------------------------- Set up Iterators ----------------------------- #
@@ -196,7 +183,7 @@ test_getter = One_hot(test_getter, vocab_dict)
 print(train_getter.iter.data['default'].shape)
 # ----------------------------- Set up Trainer ------------------------------ #
 
-trainer = bs.Trainer(bs.MomentumStep(learning_rate=0.0001, momentum=0.99), double_buffering=False)
+trainer = bs.Trainer(bs.MomentumStep(learning_rate=0.01, momentum=0.99), double_buffering=False)
 trainer.add_hook(bs.hooks.StopAfterEpoch(500))
 trainer.add_hook(bs.hooks.MonitorAccuracy("valid_getter",
                                           output="out.output",
@@ -204,7 +191,7 @@ trainer.add_hook(bs.hooks.MonitorAccuracy("valid_getter",
                                           name="validation",
                                           verbose=True))
 trainer.add_hook(bs.hooks.SaveBestNetwork("validation.accuracy",
-                                          filename='penn_corpus_best_lstm_33_cont2.hdf5',
+                                          filename='penn_corpus_best_lstm_33_new1.hdf5',
                                           name="best weights",
                                           criterion="max"))
 trainer.add_hook(bs.hooks.MonitorLayerParameters('lstm', verbose=False))
