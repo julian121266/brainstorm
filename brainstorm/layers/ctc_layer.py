321b14b129204c245fe4eca985cb19ae132ca7d1
Qwlouse <qwlouse@gmail.com>
2015-10-01 11:04:10 +0200

    adjusted the CTC layer stub to the new way layers are written.

--- brainstorm/layers/ctc_layer.py
+++ brainstorm/layers/ctc_layer.py
@@ -1,25 +1,30 @@
 #!/usr/bin/env python
 # coding=utf-8
 from __future__ import division, print_function, unicode_literals
+from collections import OrderedDict
 
-
-from brainstorm.layers.base_layer import LayerBaseImpl
+from brainstorm.layers.base_layer import BaseLayerImpl
 from brainstorm.structure.construction import ConstructionWrapper
-from brainstorm.structure.shapes import ShapeTemplate
+from brainstorm.structure.buffer_structure import (BufferStructure,
+                                                   StructureTemplate)
 
 
 def Ctc(name=None):
     return ConstructionWrapper.create('Ctc', name=name)
 
 
-class CtcLayerImpl(LayerBaseImpl):
-    inputs = {'default': ShapeTemplate('T', 'B', 'F'),
-              'labels': ShapeTemplate('T', 'B', 1)}
+class CtcLayerImpl(BaseLayerImpl):
+    expected_kwargs = {}
+    expected_inputs = {'default': StructureTemplate('T', 'B', 'F'),
+                       'labels': StructureTemplate('T', 'B', 1)}
 
-    outputs = {'loss': ShapeTemplate('B', 1)}
+    def setup(self, kwargs, in_shapes):
+        outputs = OrderedDict()
+        outputs['default'] = BufferStructure('B', 1)
+        parameters = OrderedDict()
+        internals = OrderedDict()
 
-    def _get_output_shapes(self):
-        return {'loss': ShapeTemplate('B', 1)}
+        return outputs, parameters, internals
 
     def forward_pass(self, buffers, training_pass=True):
         pass
